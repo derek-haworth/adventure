330130c184753c3f309d495b05d559733746da0d
Derek Haworth <derek_haworth@bradycorp.com>
2017-08-29 16:33:52 -0500

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+// Include gulp
+var gulp = require('gulp');
+// Include plugins
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var stripCssComments = require('gulp-strip-css-comments');
+
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+ 
+gulp.task('minify', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+  gulp.src('./assets/sass/main.scss')
+  .pipe(sass({
+    includePaths: require('node-normalize-scss').includePaths
+  }))
+  .pipe(autoprefixer())
+  .pipe(stripCssComments({preserve: false})) //preserve: false strips all comments including '/*!' important
+  .pipe(cssmin())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./css'))
+});
+
+// gulp.task('scripts', function() {
+//   return gulp.src('./assets/js/*.js')
+//     .pipe(concat('main.min.js'))
+//     .pipe(gulpIf('*.js', uglify()))
+//     .pipe(gulp.dest('./resource-bundles/CC_Emedco_Theme.resource/dist/js'));
+// });
+
+gulp.task('default', ['minify','styles']);
